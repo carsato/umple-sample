304e2f704464de2845ad681f8bf387dbdff66e34
Carlos Saport Tomás <carlos.saport@gmail.com>
2015-03-25 21:28:32 +0100

    umple files, complite.sh, add umple.jar

--- compile.sh
+++ compile.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-rm *.php src/*.php *.rb src/*.rb
-java -jar ~/opt/umple.jar  main.ump
-mv *.php src/
-mv *.rb src/
+rm *.php *.rb *.java src/*.php  src/*.rb src/*.java
+cd ump
+rm *.php *.rb *.java src/*.php  src/*.rb src/*.java
+
+java -jar ./umple.jar main.ump
+
+mv *.php ../src/
+mv *.rb  ../src/
+cd ..
